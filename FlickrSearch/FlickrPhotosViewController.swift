2df0f5468281aead9c11eba8743d5eee8e278f55
Garvan Keeley <gkeeley@slack-corp.com>
2021-06-16 11:13:45 -0400

    cleanup

--- FlickrSearch/FlickrPhotosViewController.swift
+++ FlickrSearch/FlickrPhotosViewController.swift
@@ -32,8 +32,6 @@
 
 import UIKit
 
-var vv = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
-
 final class FlickrPhotosViewController: UICollectionViewController {
   // MARK: - Properties
   private let reuseIdentifier = "FlickrCell"
@@ -41,25 +39,17 @@ final class FlickrPhotosViewController: UICollectionViewController {
   private var searches: [FlickrSearchResults] = []
   private let flickr = Flickr()
   private let itemsPerRow: CGFloat = 3
-  
-  var tf = UITextField(frame: CGRect(x: 0, y: 70, width: 100, height: 22))
-  
+
   override func viewWillAppear(_ animated: Bool) {
     collectionView!.allowsSelection = true
-    
-    view.addSubview(tf)
-    tf.backgroundColor = .yellow
-    tf.layer.borderWidth = 1
-    tf.delegate = self
   }
   
   override func viewDidAppear(_ animated: Bool) {
     flickr.searchCat() { searchResults in
       if case .success(let results) = searchResults {
         self.searches.insert(results, at: 0)
-        self.reloadData(visible: false) {
-          self.setup(enable: true)
-        }
+
+        self.collectionView!.reloadData()
       }
     }
   }
@@ -72,44 +62,9 @@ private extension FlickrPhotosViewController {
   }
 }
 
-
-// MARK: - Text Field Delegate
-extension FlickrPhotosViewController: UITextFieldDelegate {
-  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-    tf = textField
-    guard let text = textField.text, !text.isEmpty else { return true }
-    
-    let activityIndicator = UIActivityIndicatorView(style: .gray)
-    textField.addSubview(activityIndicator)
-    activityIndicator.frame = textField.bounds
-    activityIndicator.startAnimating()
-    
-    flickr.searchFlickr(for: text) { searchResults in
-      DispatchQueue.main.async {
-        activityIndicator.removeFromSuperview()
-        
-        switch searchResults {
-        case .failure(let error) :
-          print("Error Searching: \(error)")
-        case .success(let results):
-          print("Found \(results.searchResults.count) matching \(results.searchTerm) ")
-          self.searches.insert(results, at: 0)
-          self.reloadData(visible: false) {
-            self.setup(enable: true)
-          }
-        }
-      }
-    }
-    
-    textField.text = nil
-    textField.resignFirstResponder()
-    return true
-  }
-}
 var c = 0
 // MARK: - UICollectionViewDataSource
 extension FlickrPhotosViewController {
-  // 1
   override func numberOfSections(in collectionView: UICollectionView) -> Int {
     return searches.count
   }
@@ -130,51 +85,16 @@ extension FlickrPhotosViewController {
     
     return cell
   }
-  
-  func reloadData(visible: Bool, completion: @escaping () -> ()) {
-    tf.accessibilityViewIsModal = true
-    UIAccessibility.post(notification: .layoutChanged, argument: tf)
-    // DispatchQueue.main.asyncAfter(deadline: .now() + 0) {
-    UIView.animate(withDuration: 0, animations: {
-      //visible ? cv.reloadItems(at: cv.indexPathsForVisibleItems) :
-      self.collectionView!.reloadData()
-    }) {_ in
-      DispatchQueue.main.asyncAfter(deadline: .now() + 0) {
-        self.tf.accessibilityViewIsModal = false
-        //UIAccessibility.post(notification: .screenChanged, argument: nil)
-        completion()
-      }
-    }
-    //  }
-  }
-  
-  func setup(enable: Bool) {
-    //    for section in 0 ..< cv.numberOfSections {
-    //      let rowCount = cv.numberOfItems(inSection: section)
-    //      for row in 0 ..< rowCount {
-    //        let cell = cv.cellForItem(at: IndexPath(row: row, section: section))
-    //        cell?.isAccessibilityElement = enable
-    //      }
-    //    }
-  }
-  
+
   override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    print("here didSelectItemAt")
-    self.setup(enable: false)
-    reloadData(visible: true) {
-      self.setup(enable: true)
-    }
+    print("here didSelectItemAt, this will reload data")
+    collectionView.reloadData()
   }
 }
 
 // MARK: - Collection View Flow Layout Delegate
 extension FlickrPhotosViewController: UICollectionViewDelegateFlowLayout {
-  // 1
-  func collectionView(
-    _ collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    sizeForItemAt indexPath: IndexPath
-  ) -> CGSize {
+  func collectionView( _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
     let availableWidth = view.frame.width - paddingSpace
     let widthPerItem = availableWidth / itemsPerRow
@@ -187,5 +107,38 @@ extension FlickrPhotosViewController: UICollectionViewDelegateFlowLayout {
   
   func collectionView( _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
     return sectionInsets.left
+  }
+}
+
+// MARK: - Text Field Delegate
+extension FlickrPhotosViewController: UITextFieldDelegate {
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    print("Make sure to update the flickr test API key for searching to work.")
+
+    guard let text = textField.text, !text.isEmpty else { return true }
+
+    let activityIndicator = UIActivityIndicatorView(style: .gray)
+    textField.addSubview(activityIndicator)
+    activityIndicator.frame = textField.bounds
+    activityIndicator.startAnimating()
+
+    flickr.searchFlickr(for: text) { searchResults in
+      DispatchQueue.main.async {
+        activityIndicator.removeFromSuperview()
+
+        switch searchResults {
+        case .failure(let error) :
+          print("Error Searching: \(error)")
+        case .success(let results):
+          print("Found \(results.searchResults.count) matching \(results.searchTerm) ")
+          self.searches.insert(results, at: 0)
+          self.collectionView!.reloadData()
+        }
+      }
+    }
+
+    textField.text = nil
+    textField.resignFirstResponder()
+    return true
   }
 }
